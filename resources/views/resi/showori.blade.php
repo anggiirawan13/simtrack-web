688eb72017620a30a1e6e740adb4d327f5a9fd7e
anggiirawan13 <irawananggi76@gmail.com>
2024-11-02 20:10:31 +0700

    mas upi

--- resources/views/resi/showori.blade.php
+++ resources/views/resi/showori.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detail Resi</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-100">
     <div class="container mx-auto mt-10 p-6 bg-white border border-gray-200 rounded-lg shadow-lg">
